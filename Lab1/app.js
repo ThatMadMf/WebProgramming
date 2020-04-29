fde31f4f7bcdf0900045c7f8b50fda27abd8b741
Maksym Onoshko <onoshkomax@gmail.com>
2020-04-29 18:55:50 +0300

    Implemented run from docker container

--- Lab1/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send("<h1>Лабораторна робота 1 <h1><br><p>Виконав Оношко Максім</p>");
-});
-
-app.listen(8080);
