0910e154722c52fe164808edaeba55255d647b79
Max Onoshko <onoshkomax@gmail.com>
2020-05-04 16:45:27 +0300

    Moving element implemented with vanilla js

--- Lab3/js/objectLogic.js
+++ Lab3/js/objectLogic.js
@@ -1,25 +1,38 @@
-$(document).ready(function(){
-  animateDiv();
-});
+var flag = true;
 
-function makeNewPosition(){
-  
-  const windowHeight = $(window).height() - 50;
-  const windowWidth = $(window).width() - 50;
-  
-  const newYCoordinate = Math.floor(Math.random() * windowHeight);
-  const newXCoordinate = Math.floor(Math.random() * windowWidth);
-  
-  return [newYCoordinate, newXCoordinate];    
-  
+function moveDiv() {
+  setInterval(step, 1000);
+}
+function makeNewPosition() {
+
+  var width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+  var height = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
+  const newXCoordinate = Math.floor(Math.random() * width - 40);
+  const newYCoordinate = Math.floor(Math.random() * height - 40);
+
+  return [newXCoordinate, newYCoordinate];
 }
 
-function animateDiv(){
-  const newCoordinates = makeNewPosition();
-  const x = newCoordinates[0];
-  const y = newCoordinates[1];
-  $('.square').animate({ top: x, left: y }, function(){
-    animateDiv();        
-  });
-  
-};
+function step() {
+
+  if (flag === true) {
+    const object = document.getElementById("object");
+    const newCoordinates = makeNewPosition();
+    const x = newCoordinates[0];
+    const y = newCoordinates[1];
+
+    object.style.left = x;
+    object.style.top = y;
+  }
+}
+
+  function changeFlag(value) {
+    flag = value;
+  }
+
