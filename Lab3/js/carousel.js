b41f2e23ebcf471196e085007d789520c71e7a95
Max Onoshko <onoshkomax@gmail.com>
2020-05-05 02:34:03 +0300

    Implemented carousel move forward

--- Lab3/js/carousel.js
+++ Lab3/js/carousel.js
@@ -1,5 +1,20 @@
 const elements = document.getElementsByClassName("carousel-el");
+let index = 0;
 
 function test() {
-  console.log(document.getElementsByClassName("carousel-el"));
+  index = index === elements.length ? 0 : index + 1;
+  elements[previousNum(index - 1)].className = "carousel-el"
+  elements[nextNum(index)].className = "carousel-el cropped-left";
+  elements[nextNum(index + 1)].className = "carousel-el previous";
+  elements[nextNum(index + 2)].className = "carousel-el main";
+  elements[nextNum(index + 3)].className = "carousel-el next"
+  elements[nextNum(index + 4)].className = "carousel-el cropped-right";
+}
+
+function nextNum(n) {
+  return n >= elements.length ? n - elements.length : n;
+}
+
+function previousNum(n) {
+  return n < 0 ? elements.length : n; 
 }
