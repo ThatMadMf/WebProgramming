282bcc797a780b1b84912cde9fc8e1f92b35ff60
Max Onoshko <onoshkomax@gmail.com>
2020-05-05 03:15:46 +0300

    Implemented carousel move backward

--- Lab3/js/carousel.js
+++ Lab3/js/carousel.js
@@ -1,20 +1,34 @@
 const elements = document.getElementsByClassName("carousel-el");
+
 let index = 0;
 
-function test() {
+function forward() {
+  index = index === 0 ? elements.length - 1 : index -1;
+  move();
+}
+
+function back() {
   index = index === elements.length ? 0 : index + 1;
-  elements[previousNum(index - 1)].className = "carousel-el"
-  elements[nextNum(index)].className = "carousel-el cropped-left";
-  elements[nextNum(index + 1)].className = "carousel-el previous";
-  elements[nextNum(index + 2)].className = "carousel-el main";
-  elements[nextNum(index + 3)].className = "carousel-el next"
-  elements[nextNum(index + 4)].className = "carousel-el cropped-right";
+  move();
 }
 
-function nextNum(n) {
-  return n >= elements.length ? n - elements.length : n;
+function move() {
+  elements[validateNum(index - 1)].className = "carousel-el";
+  elements[validateNum(index)].className = "carousel-el cropped-left";
+  elements[validateNum(index + 1)].className = "carousel-el previous";
+  elements[validateNum(index + 2)].className = "carousel-el main";
+  elements[validateNum(index + 3)].className = "carousel-el next"
+  elements[validateNum(index + 4)].className = "carousel-el cropped-right";
 }
 
-function previousNum(n) {
-  return n < 0 ? elements.length : n; 
+function validateNum(n) {
+  if(n >= elements.length) {
+    return n - elements.length;
+  }
+  
+  if(n < 0) {
+    return n + elements.length - 1;
+  }
+
+  return n;
 }
