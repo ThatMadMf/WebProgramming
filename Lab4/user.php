0cad939442e689109652d1f362837a91c2db8d28
Max Onoshko <onoshkomax@gmail.com>
2020-05-05 21:25:24 +0300

    implemented user scenario

---
+++ Lab4/user.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+  Welcome 
+  <?php
+    
+    session_start();
+    echo "Прізвище:" . " <b>" . $_SESSION['secondName'] . "</b><br>";
+    echo "Ім'я:" . " <b>" . $_SESSION['firstName'] . "</b><br>";
+    echo "Домашній телефон:" . " <b>" . $_SESSION['phoneNumber'] . "</b><br>";
+    echo "Електронна адреса" . " <b>" . $_SESSION['email'] . "</b><br>";
+
+  ?>
+</body>
+</html>
